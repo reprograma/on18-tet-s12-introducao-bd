b9078f7a801b743b22f685a40bd9d430c63de284
Jéss <oskojess.com.br>
2022-11-04 21:58:36 -0300

    tarefa: adicionar controller, model e rota

--- projeto-sala/src/app.js
+++ projeto-sala/src/app.js
@@ -5,9 +5,14 @@ const cors = require('cors');
 const app = express();
 
 const database = require('./database/mongoConfig');
+const pacienteRoutes = require('./routes/pacienteRoutes');
+
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/paciente", pacienteRoutes);
 
 database.connect();
 
---
+++ projeto-sala/src/routes/pacienteRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers/pacienteController');
+
+router.post('/criar', controller.criarPaciente);
+router.get('/buscar', controller.buscarPaciente)
+
+module.exports = router;
+
+
